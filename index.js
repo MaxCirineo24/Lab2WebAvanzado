72a14635f2ac2a85d084e00aca0f7cee5e065e5c
MaxCirineo24 <max.cirineo@tecsup.edu.pe>
2024-04-06 21:41:54 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const routes = require('./rutas.js')
+const port = 3000
+
+app.use(routes) //app.use('/usuario',routes)
+
+//app.get('/',(req,res)=>{
+//    res.sendFile('./static/index.html',{
+//        root:__dirname
+//    })
+//})
+
+app.listen(3000)
+console.log('Server en el puerto ${3000}')
